b6668781dddc28279ba13bf61ecef98f22a8e1bf
st ts <32254365+it-spectre-ru@users.noreply.github.com>
2023-02-02 07:26:55 +0000

    create structure project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css'
 
+import Favorites from './components/Favorites'
+import Meals from './components/Meals'
+import Modal from './components/Modal'
+import Search from './components/Search'
+
 export default function App() {
   return (
     <main>
-      Meals App
+      {/* <Search /> */}
+      {/* <Favorites /> */}
+      <Meals />
+      {/* <Modal /> */}
     </main>
   )
 }
