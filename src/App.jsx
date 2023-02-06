6ba8df387347074dba4c960ce025f40acda9b7d9
st ts <32254365+it-spectre-ru@users.noreply.github.com>
2023-02-06 14:20:45 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
+import { useGlobalContext } from './context'
 import './App.css'
 
-import Favorites from './components/Favorites'
+
+
+import Search from './components/Search'
 import Meals from './components/Meals'
 import Modal from './components/Modal'
-import Search from './components/Search'
-
+import Favorites from './components/Favorites'
 export default function App() {
+  const { showModal, favorites } = useGlobalContext()
+
   return (
     <main>
+
       <Search />
-      {/* <Favorites /> */}
+      {favorites.length > 0 && <Favorites />}
+
       <Meals />
-      {/* <Modal /> */}
+      {showModal && <Modal />}
     </main>
   )
 }
--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,34 +1,33 @@
-import { useGlobalContext } from '../context';
-import { BsHandThumbsUp } from 'react-icons/bs';
-
-
+import { useGlobalContext } from '../context'
+import { BsHandThumbsUp } from 'react-icons/bs'
 const Meals = () => {
-  const { meals, loading } = useGlobalContext()
+  const { loading, meals, selectMeal, addToFavorites } = useGlobalContext();
 
   if (loading) {
-    return <section className='section'>
+    return <section className="section">
       <h4>Loading...</h4>
     </section>
   }
 
   if (meals.length < 1) {
-    return <section className='section'>
-      <h4>No meals, try again later...</h4>
+    return <section className="section">
+      <h4>No meals matched your search term. Please try again.</h4>
     </section>
   }
 
-  return <section className='section-center'>
+  return <section className="section-center">
     {meals.map((singleMeal) => {
       const { idMeal, strMeal: title, strMealThumb: image } = singleMeal
-      return <article key={idMeal} className='single-meal'>
-        <img src={image} className='img' />
+      return <article key={idMeal} className="single-meal" >
+        <img src={image} className="img" onClick={() => selectMeal(idMeal)} />
         <footer>
           <h5>{title}</h5>
-          <button className='like-btn'><BsHandThumbsUp /></button>
+          <button className='like-btn' onClick={() => addToFavorites(idMeal)}><BsHandThumbsUp /></button>
         </footer>
       </article>
     })}
   </section>
+
 }
 
-export default Meals;
+export default Meals
